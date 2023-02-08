5fb21f7b7f0d521cc87b080cb8d6a0fd66e780cc
yemoxy <abdulazeez.akande@yahoo.com>
2023-02-08 11:13:03 +0100

    added serializers

--- capstone/littlelemon/restaurant/views.py
+++ capstone/littlelemon/restaurant/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import MenuSerializer
+from .models import Menu
 
 # Create your views here.
 def index(request):
     return render(request,'index.html',{})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
